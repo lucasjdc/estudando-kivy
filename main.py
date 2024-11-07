75b064320a10560cde33ad445d980c217721096c
Lucas Costa <106778205+lucasjdc@users.noreply.github.com>
2024-11-07 12:21:53 -0300

    lista de tarefas
    consigui colocar a lista de tarefas na horizontal

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
 class Tarefas(BoxLayout):
-    def __init__(self, tarefas):
-        super().__init__()
+    def __init__(self, tarefas, **kwargs):
+        super().__init__(**kwargs)
         for tarefa in tarefas:
-            self.add_widget(Label(text=tarefa))
+            self.add_widget(Label(text=tarefa, font_size=30))
 
 class Test(App):
     def build(self):
-        return Tarefas(['Fazer compras','Buscar filho'])
+        return Tarefas(['Fazer compras','Buscar filho','Molhar a grama'], orientation='horizontal')
 
 Test().run()
